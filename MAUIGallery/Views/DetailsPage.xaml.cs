ab14935bc096f9e26885729b95f9ccc969163cd4
Vlad_Sushko <suskovladislav9@gmail.com>
2025-09-24 18:00:01 +0300

    Add Views

---
+++ MAUIGallery/Views/DetailsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Gallery.ViewModels;
+using Gallery.Models;
+
+namespace Gallery.Views
+{
+    public partial class DetailsPage : ContentPage
+    {
+        public DetailsPage(DetailsViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+    }
+}
