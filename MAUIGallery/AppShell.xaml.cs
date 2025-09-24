4515c8acc9893285fcf81ab248bca880e04de11e
Vlad_Sushko <suskovladislav9@gmail.com>
2025-09-24 18:06:55 +0300

    Add Settings

---
+++ MAUIGallery/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Gallery.Views;
+using Microsoft.Maui.Controls;
+using System.Diagnostics;
+
+namespace Gallery
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            
+                InitializeComponent();
+                
+                Routing.RegisterRoute(nameof(DetailsPage), typeof(DetailsPage));
+            
+             
+            
+        }
+    }
+}
